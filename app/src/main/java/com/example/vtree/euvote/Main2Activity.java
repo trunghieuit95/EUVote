222998917a765d93039f1bd0fe143899adaedb3c
hieunt <hieuntph03778@gmail.com>
2016-08-09 14:45:20 +0700

    Initial commit

--- app/src/main/java/com/example/vtree/euvote/Main2Activity.java
+++ app/src/main/java/com/example/vtree/euvote/Main2Activity.java
@@ -1,13 +1,16 @@
 package com.example.vtree.euvote;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class Main2Activity extends AppCompatActivity {
-
+    public ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        img = (ImageView) findViewById(R.id.imageView);
+        img.setImageResource(R.drawable.splash);
     }
 }
